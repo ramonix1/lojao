29cfa59899c6d7a754770e135508453b20552e29
ramonix1 <ramon.oliveira08@gmail.com>
2026-03-04 14:39:35 -0300

    primeira versão

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+
+app.get('/jr', (req, res) => {
+  res.render('jr')
+})
+
+
+
+app.listen(3000, () => {
+  console.log("Servidor rodando em http://localhost:3000");
+});
+
+
