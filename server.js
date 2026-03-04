1706a65040d0b9bd489f3ed4d50db7811306c347
ramonix1 <ramon.oliveira08@gmail.com>
2026-03-04 15:17:03 -0300

    ajuste porta para deploy no render

--- server.js
+++ server.js
@@ -13,10 +13,14 @@ app.get('/jr', (req, res) => {
   res.render('jr')
 })
 
+const PORT = process.env.PORT || 3000;
 
-
-app.listen(3000, () => {
-  console.log("Servidor rodando em http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
 
 
+
+//app.listen(3000, () => {
+//  console.log("Servidor rodando em http://localhost:3000");
+//});
