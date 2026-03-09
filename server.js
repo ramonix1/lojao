f21092b529895e3082691cc873ed823a52e3b51f
ramonix1 <ramon.oliveira08@gmail.com>
2026-03-09 09:41:40 -0300

    criando admin e inserindo cards de produtos

--- server.js
+++ server.js
@@ -1,24 +1,94 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
+
+
+const multer = require("multer");
+const path = require("path");
+
+//salvando imagens com multer
+
+const storage = multer.diskStorage({
+
+  destination: function (req, file, cb) {
+    cb(null, "public/images");
+  },
+
+  filename: function (req, file, cb) {
+    const nome = Date.now() + path.extname(file.originalname);
+    cb(null, nome);
+  }
+
+});
+
+const upload = multer({ storage: storage });
+
+
+
 
 app.set("view engine", "ejs");
-app.use(express.static('public'));
+app.use(express.static("public"));
 
+app.use(express.urlencoded({ extended: true }));
+
+function carregarProdutos(){
+  const data = fs.readFileSync("./data/produtos.json");
+  return JSON.parse(data);
+}
+
+// HOME
 app.get("/", (req, res) => {
-  res.render("index");
+
+  const produtos = carregarProdutos();
+
+  res.render("index", { produtos });
+
 });
 
-
-app.get('/jr', (req, res) => {
-  res.render('jr')
-})
-
-app.get("/produto", (req, res) => {
-  res.render("detail");
+// SOBRE
+app.get("/jr", (req, res) => {
+  res.render("jr");
 });
 
+// DETALHE PRODUTO
+app.get("/produto/:id", (req, res) => {
 
+  const produtos = carregarProdutos();
 
+  const produto = produtos.find(p => p.id == req.params.id);
+
+  res.render("detail", { produto });
+
+});
+
+// Salvar produto
+
+app.get("/admin", (req, res) => {
+  res.render("admin");
+});
+
+app.post("/salvar-produto", upload.single("imagem"), (req, res) => {
+
+  const produtos = carregarProdutos();
+
+  const novoProduto = {
+    id: Date.now(),
+    titulo: req.body.titulo,
+    subtitulo: req.body.subtitulo,
+    valor: req.body.valor,
+    descricao: req.body.descricao,
+    imagem: "/images/" + req.file.filename
+  };
+
+  produtos.push(novoProduto);
+
+  fs.writeFileSync(
+    "./data/produtos.json",
+    JSON.stringify(produtos, null, 2)
+  );
+
+  res.redirect("/");
+});
 
 
 const PORT = process.env.PORT || 3000;
@@ -26,9 +96,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
-
-
-
-//app.listen(3000, () => {
-//  console.log("Servidor rodando em http://localhost:3000");
-//});
